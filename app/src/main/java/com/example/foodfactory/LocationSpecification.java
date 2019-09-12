7e63de4b0f0ebe845eb4ef0e121270f1841521f0
arif-rahman-cse <smrahmanarif905@gmail.com>
2019-09-12 18:52:49 +0600

    Initial commit

---
+++ app/src/main/java/com/example/foodfactory/LocationSpecification.java
@@ -0,0 +1,14 @@
+package com.example.foodfactory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocationSpecification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_specification);
+    }
+}
